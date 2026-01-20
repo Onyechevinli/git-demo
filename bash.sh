703587bf03d1e62f618b810cd543fe4bb174fb6d
Onyechevinli <umeokolivincent@gmail.com>
2026-01-20 17:01:16 +0100

    completed bash script

--- bash.sh
+++ bash.sh
@@ -13,3 +13,17 @@ if [ ! -d "$repo_directory" ]; then
     echo "Creating directory: $repo_directory"
     mkdir "$repo_directory"
 fi
+
+cd "$repo_diectory" || exit
+
+# Intialize a new Git repository
+if [ ! -d ".git" ]; then
+    git init
+fi
+
+# Add a remote repository and fetch the latest changes
+git_repo_url="https://github.com/example/example.git" #Replace with your Git repository URL
+git remote add origin "$git_repo_url"
+git fetch origin
+
+echo "Repository setup complete. You can now start working in the $repo_diectory directory."
